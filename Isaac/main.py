41d95134a89a670d699512a617ea454c8f46e242
Isaac <kingnerdthethird@outlook.com>
2019-12-05 20:18:51 -0500

    Is a lot of files

--- Isaac/main.py
+++ Isaac/main.py
@@ -24,7 +24,7 @@ print(directories)
 
 ising.CreateDirectories(main, directories)
 
-parameters, N = [[4, 4], [10, 10], [64, 64]], 1000
+parameters, N = [[1, 1], [2, 2], [4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128], [256, 256], [512, 512], [1024, 1024]], 1000
 print(parameters)
 print(N)
 print('\n')
